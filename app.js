b1a6ef7072d5b209e4b1fc7079c5f227a5abd478
DOD98 <arlandoifrene@gmail.com>
2023-08-30 14:29:51 -0300

    Daily journal v.1.0

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
+let posts = [];
 
 const app = express();
 
@@ -24,17 +23,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //connecting to Mongoose...
-// Make your connection.
+mongoose.connect('mongodb+srv://arlando_ifrene:1998@cluster0.u1wzg0y.mongodb.net/dailyJournalDB');
+
+//Schema post title text
+const composedPostsSchema = {
+  title: String,
+  post: String
+}
+
+//model composedPost
+const ComposedPost = mongoose.model("ComposedPost",composedPostsSchema);
+
+ ComposedPost.find({}).then((result) => {
+  result.forEach((rslt => {
+
+    
+     const postTitle_Text = {
+     title: rslt.title,
+     post: rslt.post,
+     };
+
+  posts.push(postTitle_Text);
+
+  })); 
+});
 
 app.get( "/", (req, res) => {
 
-    
-    res.render("home" , {
+      res.render("home" , {
         homeStartingContent : homeStartingContent , 
         posts : posts 
         
       });
     
+     
       
 });
 
@@ -59,11 +81,15 @@ app.post( "/compose", (req, res) => {
     
   };
   
-
-
   posts.push(postTitle_Text);
-  
 
+  const composedPost = new ComposedPost({
+    title: req.body.titlePost,
+    post: req.body.postText,
+  });
+
+  composedPost.save();
+  
   res.redirect("/");
   
 });
